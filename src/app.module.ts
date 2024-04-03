edb71f55196d532b9502ef63d9a5c75b7034143e
utility-libraries-star <114432586+utility-libraries-star@users.noreply.github.com>
2024-04-03 10:09:04 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FormatController } from './format/format.controller';
 import { FormatService } from './format/format.service';
+import { MockRestApiController } from './mock-rest-api/mock-rest-api.controller';
+import { MockRestApiService } from './mock-rest-api/mock-rest-api.service';
 
 @Module({
   imports: [],
-  controllers: [FormatController],
-  providers: [FormatService],
+  controllers: [FormatController, MockRestApiController],
+  providers: [FormatService, MockRestApiService],
 })
 export class AppModule {}
