125cda82ab1cb05af50e3c038068e211e59c00bd
utility-libraries-star <114432586+utility-libraries-star@users.noreply.github.com>
2024-04-10 12:58:08 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { FormatController } from './format/format.controller';
 import { FormatService } from './format/format.service';
 import { MockRestApiController } from './mock-rest-api/mock-rest-api.controller';
 import { MockRestApiService } from './mock-rest-api/mock-rest-api.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ProxyController } from './proxy/proxy.controller';
+import { ProxyService } from './proxy/proxy.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [FormatController, MockRestApiController],
-  providers: [FormatService, MockRestApiService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [FormatController, MockRestApiController, ProxyController],
+  providers: [FormatService, MockRestApiService, ProxyService],
 })
 export class AppModule {}
