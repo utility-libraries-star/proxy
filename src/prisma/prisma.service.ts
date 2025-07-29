a7e9dc6d4a1785dd38a923f59dc4dbf2a0839055
Alexey Movchan <a.movchan@elfsight.com>
2025-07-29 15:02:29 +0300

    UPD

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const certContent = process.env.SSL_CERT.replace(/\\n/g, '\n');
+    if (certContent) {
+      const certPath = join('/tmp', 'ca.pem');
+      writeFileSync(certPath, certContent, 'utf-8');
+    }
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
