6eefda31bbcd4336da000e47b17dd76e39e97ae8
Alexey Movchan <a.movchan@elfsight.com>
2025-07-29 11:51:21 +0300

    UPD

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-let appInstance;
-
-export default async function handler(req, res) {
-  if (!appInstance) {
-    const app = await NestFactory.create(AppModule);
-    await app.init();
-    appInstance = app.getHttpAdapter().getInstance(); // express
-  }
-
-  return appInstance(req, res);
+export async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
+bootstrap();
