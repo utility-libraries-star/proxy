1c47661942204e7ed73b14368f9cdef13854e9f8
Alexey Movchan <a.movchan@elfsight.com>
2025-07-29 11:12:09 +0300

    UPD

--- dist/rss-parser/rss-parser.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { RssParserService } from './rss-parser.service';
-export declare class RssParserController {
-    private readonly rssService;
-    constructor(rssService: RssParserService);
-    getRss(url: string, clearCache?: string): Promise<any>;
-}
--- dist/widget/widget.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { WidgetService } from './widget.service';
-import { Response } from 'express';
-export declare class WidgetController {
-    private readonly widgetService;
-    constructor(widgetService: WidgetService);
-    getWidget(widgetID: string, res: Response): void;
-}
